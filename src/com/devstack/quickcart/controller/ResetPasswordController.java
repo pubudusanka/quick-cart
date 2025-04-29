3374c7ab6b3706b30ca697591d50cea147bac111
Pubudu Sanka <pubudusanka79@gmail.com>
2025-04-29 16:57:03 +0530

    Create ResetPasswordController.java

---
+++ src/com/devstack/quickcart/controller/ResetPasswordController.java
@@ -0,0 +1,16 @@
+package com.devstack.quickcart.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class ResetPasswordController {
+    public TextField txtNewPswd;
+    public TextField txtNewPswdReEnter;
+
+    public void btnChangePswd(ActionEvent actionEvent) {
+
+    }
+
+    public void showPswd(ActionEvent actionEvent) {
+    }
+}
