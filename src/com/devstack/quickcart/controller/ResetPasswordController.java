fff54c85410c3d12d10bdec1166a339881048c00
Pubudu Sanka <pubudusanka79@gmail.com>
2025-05-01 11:57:04 +0530

    Update ResetPasswordController.java

--- src/com/devstack/quickcart/controller/ResetPasswordController.java
+++ src/com/devstack/quickcart/controller/ResetPasswordController.java
@@ -1,14 +1,38 @@
 package com.devstack.quickcart.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ResetPasswordController {
     public TextField txtNewPswd;
     public TextField txtNewPswdReEnter;
+    public AnchorPane container;
 
     public void btnChangePswd(ActionEvent actionEvent) {
+        setUi("LoginForm");
+    }
 
+    private void setUi(String location) {
+        URL resource = getClass().getResource("../view/" + location + ".fxml");
+        Parent parent = null;
+        try {
+            parent = FXMLLoader.load(resource);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) container.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle(location);
+        stage.show();
     }
 
     public void showPswd(ActionEvent actionEvent) {
