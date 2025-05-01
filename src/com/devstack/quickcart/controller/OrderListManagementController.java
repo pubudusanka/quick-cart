15b8143ca981ff6051326e7c2a23506348109b89
Pubudu Sanka <pubudusanka79@gmail.com>
2025-05-01 21:46:41 +0530

    Create OrderListManagementController.java

---
+++ src/com/devstack/quickcart/controller/OrderListManagementController.java
@@ -0,0 +1,40 @@
+package com.devstack.quickcart.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class OrderListManagementController {
+    public AnchorPane container;
+    public TableView orderTable;
+
+    private void setUi(String location) {
+        URL resource = getClass().getResource("../view/" + location + ".fxml");
+        Parent parent = null;
+        try {
+            parent = FXMLLoader.load(resource);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) container.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle(location);
+        stage.show();
+    }
+
+    public void orderListBackButton(MouseEvent mouseEvent) {
+        setUi("Statistics");
+    }
+
+    public void btnPrintData(ActionEvent actionEvent) {
+    }
+}
